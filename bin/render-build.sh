91ecc41e0fce06f5766e6fef9c436ba825bf7429
RENAGE-DEV <diego.gonzalez30@outlook.com>
2025-01-15 09:31:20 -0300

    Fix permissions and update build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,16 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+
+# Setup database
+bundle exec rake db:migrate
+bundle exec rake db:seed
+
+# Asset compilation
+bundle exec rake assets:clean
+bundle exec rake assets:precompile
+
+# Ensure all changes are saved
+bundle exec rake assets:clean
